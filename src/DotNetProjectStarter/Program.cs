8343e6535035a851f0b260f90fcc1f6efa9aa1f7
Youssef1313 <youssefvictor00@gmail.com>
2025-12-28 23:31:59 +0100

    Invoke command correctly

--- src/DotNetProjectStarter/Program.cs
+++ src/DotNetProjectStarter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.CommandLine;
+using System.Threading.Tasks;
 
 var nameOption = new Option<string>("--name")
 {
@@ -22,13 +23,17 @@ var rootCommand = new RootCommand("Creates a new .NET project template");
 rootCommand.Options.Add(nameOption);
 rootCommand.Options.Add(outputDirectoryOption);
 rootCommand.Options.Add(nugetUsernameOption);
+rootCommand.SetAction(parseResult =>
+{
+    var name = parseResult.GetValue(nameOption);
+    var outputDirectory = parseResult.GetValue(outputDirectoryOption);
+    var nugetUsername = parseResult.GetValue(nugetUsernameOption);
+
+    var options = new TemplateGenerationOptions(name, outputDirectory, nugetUsername);
+
+    var generator = new LibraryTemplateGenerator();
+    return Task.FromResult(generator.Generate(options) ? 0 : 1);
+});
 
 var parseResult = rootCommand.Parse(args);
-var name = parseResult.GetValue(nameOption);
-var outputDirectory = parseResult.GetValue(outputDirectoryOption);
-var nugetUsername = parseResult.GetValue(nugetUsernameOption);
-
-var options = new TemplateGenerationOptions(name, outputDirectory, nugetUsername);
-
-var generator = new LibraryTemplateGenerator();
-generator.Generate(options);
+return await parseResult.InvokeAsync();
